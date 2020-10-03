33f8deb5fedce6accbf0dab6b3d9275e167e6b47
r.rahmanipour <r.rahmanipour@gmail.com>
2020-10-03 14:01:47 +0330

    IntroScreen created and YekanBakh font added

---
+++ src/Screens/confirmationScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ConfirmationScreen = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 48}}>ConfirmationScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ConfirmationScreen;
